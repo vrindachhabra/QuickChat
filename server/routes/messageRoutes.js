581d710a4069b312050a5e69f0cfa6bcd26052f8
vrindachhabra <vrindchh@gmail.com>
2026-02-01 09:35:44 +0530

    Message Route

---
+++ server/routes/messageRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.js";
+import { getMessages, getUsersForSidebar, markMessageAsSeen } from "../controllers/messageController.js";
+const messageRouter = express.Router();
+
+messageRouter.get('/users', protectRoute, getUsersForSidebar)
+messageRouter.get('/:id', protectRoute, getMessages)
+messageRouter.put('mark/:id', protectRoute, markMessageAsSeen)
+
+export default messageRouter
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
 //Creating express app using http server
 const app = express();
@@ -16,6 +17,7 @@ app.use(cors());
 //Routes Setup
 app.use('/api/status', (req, res) => res.send("Server is live"))
 app.use("/api/auth", userRouter)
+app.use('/api/messages', messageRouter)
 
 //Connect to mongodb
 await connectDB();
