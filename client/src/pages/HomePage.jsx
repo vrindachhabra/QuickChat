f332501e5a04f693712988229b46a86ca11f1112
vrindachhabra <vrindchh@gmail.com>
2026-02-07 15:54:44 +0530

    Sidebar Users display

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
   return (
     <div className='border w-full h-screen sm:px-[12%] sm:py-[3%]'>
       <div className={`backdrop-blur-sm border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
-        <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+        <Sidebar/>
         <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
         <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
       </div>
