3b7df8ab0d33157035ab161f830a42d59ff71a8b
vrindachhabra <vrindchh@gmail.com>
2026-02-09 12:05:50 +0530

    completed

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import assets, { imagesDummyData } from '../assets/assets'
+import { useContext } from 'react';
+import { ChatContext } from '../../context/ChatContext.jsx';
+import { AuthContext } from '../../context/AuthContext.jsx';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-const RightSideBar = ({selectedUser}) => {
+const RightSideBar = () => {
+  const {selectedUser, messages} = useContext(ChatContext);
+  const {logout, onlineUsers} = useContext(AuthContext);
+  const [msgImages, setMsgImages] = useState([]);
+
+  //get all images from messages and set them in msgImages state
+  useEffect(() => {
+    setMsgImages(
+      messages.filter(msg => msg.image).map(msg => msg.image) //map function extracts image url from message object and creates new array of image urls
+    )  
+  }, [messages])
+
   return selectedUser && (
     <div className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${selectedUser ? 'max-md:hidden' : ''}`}>
 
       <div className='pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto'>
         <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" className='w-20 aspect-square rounded-full' />
         <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
-          <p className='w-2 h-2 rounded-full bg-green-500'></p>
+          {onlineUsers.includes(selectedUser._id) && <p className='w-2 h-2 rounded-full bg-green-500'></p>}
         {selectedUser.fullName}
         </h1>
         <p className='px-10 mx-auto'>{selectedUser.bio}</p>
@@ -19,7 +35,7 @@ const RightSideBar = ({selectedUser}) => {
       <div className='px-5 text-xs'>
         <p>Media</p>
         <div className='mt-2 max-h-50 overflow-y-scroll grid grid-cols-2 gap-4 opacity-80'>
-          {imagesDummyData.map((url, index) => (
+          {msgImages.map((url, index) => (
             <div key={index} onClick={() => window.open(url)} className='cursor-pointer rounded'>
               <img src={url} alt="" className='h-full rounded-md'/>
             </div>
@@ -28,7 +44,7 @@ const RightSideBar = ({selectedUser}) => {
       </div>
 
       {/* logout  */}
-      <button className='absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-linear-to-r from-purple-400 to-violet-600 text-white border-none text-sm py-2 px-20 rounded-full cursor-pointer'>
+      <button onClick={() => logout()} className='absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-linear-to-r from-purple-400 to-violet-600 text-white border-none text-sm py-2 px-20 rounded-full cursor-pointer'>
         Logout
       </button>
     </div>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react'
-import Sidebar from '../components/Sidebar'
+import Sidebar from '../components/Sidebar.jsx'
 import ChatContainer from '../components/ChatContainer.jsx'
 import RightSidebar from '../components/RightSidebar.jsx'
+import { useContext } from 'react'
+import { ChatContext } from '../../context/ChatContext.jsx'
 
 const HomePage = () => {
-  const [selectedUser, setSelectedUser] = useState(false);
+  const {selectedUser} = useContext(ChatContext);
   return (
     <div className='border w-full h-screen sm:px-[12%] sm:py-[3%]'>
       <div className={`backdrop-blur-sm border-2 border-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
         <Sidebar/>
         <ChatContainer/>
-        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+        <RightSidebar/>
       </div>
     </div>
   )
