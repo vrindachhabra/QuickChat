0951f13ff2b817f854b862a2e6e49061debd8f60
vrindachhabra <vrindchh@gmail.com>
2026-02-09 11:50:20 +0530

    Sidebar updates in real-time

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -42,7 +42,7 @@ const Sidebar = () => {
         {/* User Profiles  */}
         <div className='flex flex-col'>
           {filteredUsers.map((user, index) => (
-            <div onClick={() => {setSelectedUser(user)}} key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#282142]/50'}`}>
+            <div onClick={() => {setSelectedUser(user), setUnseenMessages(prev => ({...prev, [user._id]: 0}))}} key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#282142]/50'}`}>
               <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-8.75 aspect-square rounded-full'/>
               <div className='flex flex-col leading-5'>
                 <p>{user.fullName}</p>
